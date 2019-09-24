3fb9a1f17f52e16d5f9daf128ae9fd6ed3ee2c21
andeerowe <andeerowe@gmail.com>
2019-09-24 14:58:57 -0600

    Finished App5 -AR

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Image from './Components/ImageComponent'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <Image 
+      img={'https://assets.vogue.com/photos/5d2f42b4e529c100082ac096/master/pass/00-promo-image-amalfi-coast-italy-travel-guide.jpg'}/>
       </div>
     );
   }
