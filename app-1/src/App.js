dd1b39a7529252dc0ed3a39682b81e312da1e8ad
andeerowe <andeerowe@gmail.com>
2019-09-24 13:57:11 -0600

    Completed App1 -AR

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,18 +1,26 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor (){
+    super ()
+
+    this.state = {
+      userInput:''
+    }
+  }
+
+  updateWords = (val) => {
+    this.setState({
+      userInput: val
+    })
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange={(e) => this.updateWords(e.target.value)}/>
+        <h5>{this.state.userInput}</h5>
       </div>
     );
   }
