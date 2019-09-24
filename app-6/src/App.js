7ee0939663c20efd98470b8ba56475a1344d19ef
andeerowe <andeerowe@gmail.com>
2019-09-24 16:20:11 -0600

    Completed App6 -AR

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
+import Todo from './Components/Todo'
 
 class App extends Component {
+
+constructor () {
+  super ()
+
+  this.state = {
+    todoList: [],
+    item: ''
+  }
+}
+
+updateItem = (val) => {
+  this.setState({
+    item: val
+  })
+  // console.log(this.state.todoList)
+}
+
+updateList = () => {
+  this.setState({
+    todoList: [...this.state.todoList, this.state.item],
+    item: ''
+  })
+}
+
   render() {
+
+    let list = this.state.todoList.map((e,i) => {
+      return <Todo key={i} task={e}/>
+  
+    })
+
+    console.log(this.state.todoList)
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <h1>My To-Do List</h1>
+
+        <div>
+        <input onChange={(e)=>this.updateItem(e.target.value)}/>
+        <button onClick={() => this.updateList()}>Add</button>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <br />
+        {list}
       </div>
     );
   }
---
+++ app-6/src/Components/Todo.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+
+class Todo extends Component {
+    render (){
+        // return (
+        // <span>
+        // {this.props.task}
+        
+        // </span>
+        // )
+
+        return <p>{this.props.task}</p>
+                
+            
+        
+    }
+    
+}
+
+export default Todo
