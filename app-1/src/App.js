9498ce980455d37eb2f63001d20da9853cd51d3a
andeerowe <andeerowe@gmail.com>
2019-10-22 14:15:42 -0600

    Finished Apps 1 & 2

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,29 +1,45 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 // import logo from "./logo.svg";
+
+
+// class App extends Component {
+//   constructor (){
+//     super ()
+
+//     this.state = {
+//       userInput:''
+//     }
+//   }
+
+//   updateWords = (val) => {
+//     this.setState({
+//       userInput: val
+//     })
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//         <input onChange={(e) => this.updateWords(e.target.value)}/>
+//         <h5>{this.state.userInput}</h5>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, { useState } from 'react'
 import "./App.css";
 
-class App extends Component {
-  constructor (){
-    super ()
-
-    this.state = {
-      userInput:''
-    }
-  }
-
-  updateWords = (val) => {
-    this.setState({
-      userInput: val
-    })
-  }
-  render() {
-    return (
-      <div className="App">
-        <input onChange={(e) => this.updateWords(e.target.value)}/>
-        <h5>{this.state.userInput}</h5>
-      </div>
-    );
-  }
+const App = () => {
+  const [text, setText] = useState('')
+return(
+  <div>
+    <input onChange={(e) => setText(e.target.value)}/>
+    {text}
+  </div>
+  
+)
 }
 
-export default App;
+export default App
--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,28 +1,46 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
 
-class App extends Component {
-  constructor () {
-    super ()
+// class App extends Component {
+//   constructor () {
+//     super ()
 
-    this.state = {
-      dataArr:['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
-    }
-  }
-  render() {
-    return (
-      <div className="App">
-       <ul>
-         <li><h2>{this.state.dataArr[0]}</h2></li>
-         <li><h2>{this.state.dataArr[1]}</h2></li>
-         <li><h2>{this.state.dataArr[2]}</h2></li>
-         <li><h2>{this.state.dataArr[3]}</h2></li>
-         <li><h2>{this.state.dataArr[4]}</h2></li>
-       </ul>
-      </div>
-    );
-  }
+//     this.state = {
+//       dataArr:['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+//     }
+//   }
+//   render() {
+//     return (
+//       <div className="App">
+//        <ul>
+//          <li><h2>{this.state.dataArr[0]}</h2></li>
+//          <li><h2>{this.state.dataArr[1]}</h2></li>
+//          <li><h2>{this.state.dataArr[2]}</h2></li>
+//          <li><h2>{this.state.dataArr[3]}</h2></li>
+//          <li><h2>{this.state.dataArr[4]}</h2></li>
+//        </ul>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+import React, {useState} from 'react'
+
+const App = () => {
+  const [list, setList] = useState(['spaghetti','ice cream','sushi','bologna','cheese'])
+  return(
+    <div>
+      <ul>
+      {list.map((e, i) => {
+        return (
+          <h2 key={i}>{e}</h2>
+        )
+      })}
+      </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
