4275a80189071862a4fe7ab060e449998e82842e
andeerowe <andeerowe@gmail.com>
2019-10-22 14:56:24 -0600

    Completed Apps 1-4

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,38 +1,56 @@
-import React, { Component } from "react";
-import "./App.css";
+// import React, { Component } from "react";
+// import "./App.css";
 
-class App extends Component {
-  constructor (){
-    super ()
+// class App extends Component {
+//   constructor (){
+//     super ()
 
-    this.state ={
-      results: '',
-      dataArr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
+//     this.state ={
+//       results: '',
+//       dataArr: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese'],
       
-    }
-  }
+//     }
+//   }
 
-  updateUserInput = (val) => {
-      this.setState({
-      results: val,
-    }) 
-  }
+//   updateUserInput = (val) => {
+//       this.setState({
+//       results: val,
+//     }) 
+//   }
 
-  render() {
-     let foodsToDisplay = this.state.dataArr
-    .filter((element, index) => {
-      return element.includes(this.state.results);
-    })
-    .map((element, index) => {
-      return <h2 key={index}>{element}</h2>;
-    });
-    return (
-      <div className="App">
-        <input onChange={(e) => this.updateUserInput(e.target.value)} type="text"/>
-        {foodsToDisplay}
-      </div>
-    );
-  }
+//   render() {
+//      let foodsToDisplay = this.state.dataArr
+//     .filter((element, index) => {
+//       return element.includes(this.state.results);
+//     })
+//     .map((element, index) => {
+//       return <h2 key={index}>{element}</h2>;
+//     });
+//     return (
+//       <div className="App">
+//         <input onChange={(e) => this.updateUserInput(e.target.value)} type="text"/>
+//         {foodsToDisplay}
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+import React, {useState} from 'react'
+
+const App = () => {
+  const [list] = useState(['spaghetti', 'ice cream','sushi', 'bologna','cheese'])
+  const [search, setSearch] = useState('')
+  const display = list.filter(e => e.includes(search))
+  return(
+    <div>
+      <input onChange={e => setSearch(e.target.value)}/>
+      {display.map((e,i) => {
+        return <h2 key={i}>{e}</h2>
+      })}
+    </div>
+  )
 }
 
-export default App;
+export default App
--- app-4/src/Components/Login.js
+++ app-4/src/Components/Login.js
@@ -1,43 +1,19 @@
-import React, {Component} from 'react'
+import React, {useState} from 'react'
 
-class Login extends Component {
-    constructor (){
-        super()
-
-        this.state = {
-            username: '',
-            password: ''
-        }
-    }
-
-    updateUsername = (val) => {
-        this.setState({
-            username: val
-        })
-    }
-
-    updatePassword = (val) => {
-        this.setState({
-            password: val
-        })
-    }
-
-    alertFunction = () => {
-        alert(`USERNAME: ${this.state.username}    PASSWORD: ${this.state.password}`)
-    }
-
-    render (){
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
         
         return (
             <div>
                 <label>Username:</label>
-                <input onChange={(e) => this.updateUsername(e.target.value)}/>
+                <input onChange={(e) => setUsername(e.target.value)}/>
                 <label>Password:</label>
-                <input onChange={(e) => this.updatePassword(e.target.value)}/>
-                <button onClick={() => this.alertFunction()}>Login</button>
+                <input onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={() => alert(`USERNAME: ${username}    PASSWORD: ${password}`)}>Login</button>
             </div>
         )
-    }
+    
 }
 
 export default Login
